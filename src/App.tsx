48e06312ead27d131ae3d2fddd7eec906bb48520
lisapatrikeevna <38157157+lisapatrikeevna@users.noreply.github.com>
2021-01-22 18:36:10 +0200

    Update App.tsx
    delete commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ function App() {
             <Header/>
             <Switch>
                 <Route path={'/'} exact render={()=><Redirect to={PATH.CURRENSYRATES}/>}/>
-                {/*<Route path={'/'} exact render={()=>{<CurrencyEContainer/>}}/>*/}
-                <Route path={PATH.CALCULATOR} exact render={()=><CurrencyEContainer/>}/>
+                <Route path={PATH.CALCULATOR} render={()=><CurrencyEContainer/>}/>
                 <Route path={PATH.CURRENSYRATES} render={()=><CurrencyRates/>}/>
             </Switch>
         </div>
