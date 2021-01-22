67bb14a6bf4d4ec0dc2f5c05b02845ef90412eb8
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2021-01-22 19:25:46 +0200

    test1

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import { store } from './redux/state';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
